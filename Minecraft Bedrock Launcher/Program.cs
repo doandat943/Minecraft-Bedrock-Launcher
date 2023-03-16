ed4eb9d91abfa05022e5a892eef74b665c00737b
doandat943 <doandat943@kamvdta.xyz>
2023-03-17 02:26:45 +0700

    1.0.3.1

--- Minecraft Bedrock Launcher/Program.cs
+++ Minecraft Bedrock Launcher/Program.cs
@@ -1,22 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Minecraft_Bedrock_Launcher
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        private const int SW_RESTORE = 9;
+
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            using (Mutex mutex = new Mutex(false, "ABC-unique-mutex"))
+            {
+                if (!mutex.WaitOne(TimeSpan.Zero, true))
+                {
+                    Process currentProcess = Process.GetCurrentProcess();
+                    foreach (Process process in Process.GetProcessesByName(currentProcess.ProcessName))
+                    {
+                        if (process.Id != currentProcess.Id && process.MainWindowHandle != IntPtr.Zero)
+                        {
+                            ShowWindow(process.MainWindowHandle, SW_RESTORE);
+                            SetForegroundWindow(process.MainWindowHandle);
+                            break;
+                        }
+                    }
+                    return;
+                }
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm());
+
+                mutex.ReleaseMutex();
+            }
         }
     }
 }
